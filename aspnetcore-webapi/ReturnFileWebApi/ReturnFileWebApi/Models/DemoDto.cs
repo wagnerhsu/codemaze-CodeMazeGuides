72df52419957dc9f4d91bffa9e6b6f6575975ba9
Wagner Xu <wagnerhsu@hotmail.com>
2023-05-24 16:14:40 +0800

    feat: add File demo

---
+++ aspnetcore-webapi/ReturnFileWebApi/ReturnFileWebApi/Models/DemoDto.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) xxx, 2022. All rights reserved.
+
+namespace ReturnFileWebApi.Models
+{
+    public class DemoDto
+    {
+        public string Name { get; set; }
+    }
+}
--- aspnetcore-webapi/ReturnFileWebApi/ReturnFileWebApi/Pages/Index.cshtml.cs
+++ aspnetcore-webapi/ReturnFileWebApi/ReturnFileWebApi/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Net.Http.Headers;
+using ReturnFileWebApi.Models;
 
 namespace ReturnFileWebApi.Pages;
 
@@ -14,6 +17,18 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+    }
 
+    public async Task<IActionResult> OnGetHandleStringAsync()
+    {
+        await Task.CompletedTask;
+        return new OkObjectResult(new
+            DemoDto { Name = "Wagner" });
+    }
+
+    public async Task<IActionResult> OnGetHandleFileAsync()
+    {
+        await Task.CompletedTask;
+        return new FileContentResult(Encoding.UTF8.GetBytes("Wagner"), "text/html"){ FileDownloadName = "FileDemo.key"};
     }
 }
